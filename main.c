8e9477fc75f478a05f627949e71b65b4fc416d17
o0sanket0o <sanketchugh5@gmail.com>
2024-08-22 03:46:18 +0530

    Implemented min heap and created a binary Tree

--- main.c
+++ main.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+#include "tree.c"
 
 int main() {
-    char str[100];
-    printf("Enter the string you want to encode:");
-    scanf("%[^\n]s",str);
-    printf("You entered: %s\n",str);
-    
-    return 0;
+	char a[100];
+	scanf("%[^\n]s",a);
+	printf("%s\n",a);
+	userPair arr[128];
+	Tree* store[128];
+	for(int i = 0; i < 128; i++){
+		arr[i].charCount = 0;
+		arr[i].value = i;
+	}
+	for(int i = 0; a[i] != '\0'; ++i){
+		// printf("%d\n", i);
+		arr[a[i]].charCount++;
+		arr[a[i]].value = a[i];
+	} 
+	int ind = 0, n = 0;
+	for(int i = 0; i < 128; i++){
+		if(arr[i].charCount != 0){
+			Tree* temp = create(arr[i].charCount, arr[i].value, NULL, NULL);
+			store[ind++] = temp;
+			n++;
+		}
+	}
+	Tree* root = combine(store, n);
+	Tree* temp = (Tree*) malloc (sizeof(Tree));
+	temp = root;
+	printf("\nPreorder:\n");
+	preorder(temp);
+	temp = root;
+	printf("\nInorder:\n");
+	inorder(temp);
+	return 0;
 }
