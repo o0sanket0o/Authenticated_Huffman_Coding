f7ca9410a56907f54cccae982086004bfcf850e6
o0sanket0o <sanketchugh5@gmail.com>
2024-09-04 17:22:30 +0530

    Updated main file

--- huffman.c
+++ huffman.c
@@ -1,41 +1,61 @@
 #include <stdio.h>
 #include <string.h>
-#include "tree.c"
+#include "tree.c"  // Include the Huffman tree implementation
 #include <sys/time.h>
 #include <stdlib.h>
 
-void implementHuffman(char *a){
-//	printf("We got %s.\n", a);
-	struct timeval before;
-	gettimeofday(&before, NULL);
-	long long t1 = before.tv_sec * 1000 + before.tv_usec / 1000;
-	userPair arr[128];
-	Tree* store[128];
-	for(int i = 0; i < 128; i++){
-		arr[i].charCount = 0;
-		arr[i].value = i;
-	}
-	for(int i = 0; a[i] != '\0'; ++i){
-		arr[a[i]].charCount++;
-		arr[a[i]].value = a[i];
-	} 
-	struct timeval after;
-	int ind = 0, n = 0;
-	for(int i = 0; i < 128; i++){
-		if(arr[i].charCount != 0){
-			Tree* temp = create(arr[i].charCount, arr[i].value, NULL, NULL);
-			store[ind++] = temp;
-			n++;
-		}
-	}
-	Tree* root = combine(store, n);
-	Tree* temp = (Tree*) malloc (sizeof(Tree));
-	gettimeofday(&after, NULL);
-	long long t2 = after.tv_sec * 1000 + after.tv_usec / 1000;
-	temp = root;
-	temp = root;
-	char* ans = (char *) malloc(100 * sizeof(char));
-	ind = 0;
-	preorder(temp, ans, ind);
-	printf("Time taken to encode the string is : %llums.\n", t2 - t1);
+// Function to implement Huffman encoding on the input string `a`
+void implementHuffman(char *a) {
+    struct timeval before;  // Struct to hold the start time of the encoding process
+    gettimeofday(&before, NULL);  // Get the current time before starting the process
+    long long t1 = before.tv_sec * 1000 + before.tv_usec / 1000;  // Convert time to milliseconds
+
+    userPair arr[128];  // Array to store frequency of each character (ASCII values from 0 to 127)
+    Tree* store[128];   // Array to store nodes for Huffman tree
+
+    // Initialize the character frequency array
+    for (int i = 0; i < 128; i++) {
+        arr[i].charCount = 0;
+        arr[i].value = i;
+    }
+
+    // Count the frequency of each character in the input string
+    for (int i = 0; a[i] != '\0'; ++i) {
+        arr[a[i]].charCount++;
+        arr[a[i]].value = a[i];
+    }
+
+    struct timeval after;  // Struct to hold the end time of the frequency counting process
+    int ind = 0, n = 0;
+
+    // Create nodes for each character with non-zero frequency
+    for (int i = 0; i < 128; i++) {
+        if (arr[i].charCount != 0) {
+            Tree* temp = create(arr[i].charCount, arr[i].value, NULL, NULL);  // Create a new tree node
+            store[ind++] = temp;  // Store the node in the array
+            n++;
+        }
+    }
+
+    // Combine nodes to create the Huffman tree
+    Tree* root = combine(store, n);
+    Tree* temp = (Tree*) malloc (sizeof(Tree));
+
+    gettimeofday(&after, NULL);  // Get the current time after creating the tree
+    long long t2 = after.tv_sec * 1000 + after.tv_usec / 1000;  // Convert time to milliseconds
+
+    temp = root;  // Set temp to root (root of Huffman tree)
+    char* ans = (char *) malloc(100 * sizeof(char));  // Allocate memory for storing encoded string
+    ind = 0;
+
+    // Traverse the Huffman tree in preorder and generate the encoded string
+    preorder(temp, ans, ind);
+
+    // Print the time taken to encode the string
+    printf("Time taken to encode the string is : %llums.\n", t2 - t1);
+
+    // Free allocated memory (if needed in the future implementation)
+    free(ans);
+    free(temp);
 }
+
--- main.c
+++ main.c
@@ -1,165 +1,47 @@
+#include "customHeader.h"  
+#include "huffman.c"      
 #include <stdio.h>
-#include <string.h>
-#include <CommonCrypto/CommonCrypto.h>
-#include <stdbool.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include "huffman.c"
-#include "generatePass.c"
-#include "history.c"
-
-
-void hashPassword(const char *password, unsigned char hash[CC_SHA256_DIGEST_LENGTH]) {
-    CC_SHA256_CTX sha256;
-    CC_SHA256_Init(&sha256);
-    CC_SHA256_Update(&sha256, password, strlen(password));
-    CC_SHA256_Final(hash, &sha256);
-}
-
-void printHash(const unsigned char hash[CC_SHA256_DIGEST_LENGTH]) {
-    for (int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++) {
-        printf("%02x", hash[i]);
-    }
-    printf("\n");
-}
-
-int compareHashes(const unsigned char hash1[CC_SHA256_DIGEST_LENGTH], const unsigned char hash2[CC_SHA256_DIGEST_LENGTH]) {
-    return memcmp(hash1, hash2, CC_SHA256_DIGEST_LENGTH) == 0;
-}
-
-void hexStringToBytes(const char *hexString, unsigned char *hash) {
-    for (int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++) {
-        sscanf(hexString + 2 * i, "%2hhx", &hash[i]);
-    }
-}
-
-bool readHashFromFile(const char *filename, unsigned char hash[CC_SHA256_DIGEST_LENGTH]) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("Failed to open file");
-        return false;
-    }
-
-    char hexHash[2 * CC_SHA256_DIGEST_LENGTH + 1];
-    if (fgets(hexHash, sizeof(hexHash), file) != NULL) {
-        hexHash[strcspn(hexHash, "\n")] = '\0';  
-        hexStringToBytes(hexHash, hash);
-        fclose(file);
-        return true;
-    }
-
-    fclose(file);
-    return false;
-}
-
-void writeHashToFile(const char *filename, const unsigned char hash[CC_SHA256_DIGEST_LENGTH]) {
-    FILE *file = fopen(filename, "w");
-    if (file == NULL) {
-        perror("Failed to open file for writing");
-        return;
-    }
-
-    for (int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++) {
-        fprintf(file, "%02x", hash[i]);
-    }
-
-    fclose(file);
-}
-
-bool checkPass(char *userPassword) {
-    unsigned char hash[CC_SHA256_DIGEST_LENGTH];
-    hashPassword(userPassword, hash);
-//    printf("Hash of the entered password: ");
-//    printHash(hash);
-
-    unsigned char hashToVerify[CC_SHA256_DIGEST_LENGTH];
-    if (!readHashFromFile("pass.txt", hashToVerify)) {
-        printf("Failed to read hash from file.\n");
-        return false;
-    }
-
-    if (compareHashes(hash, hashToVerify)) {
-        printf("Password verification successful.\n");
-        return true;
-    } else {
-        printf("Wrong password.\n");
-        printf("Enter 1 to change the password.\nEnter 2 to exit the program.\n");
-        int num;
-        scanf("%d", &num);
-        getchar(); 
-        if(num == 1){
-            int choice;
-            char *newPassword = NULL;
-            printf("Enter 1 to generate a random password.\nEnter 2 to create your own password.\n");
-            scanf("%d", &choice);
-            getchar();  // Consume leftover newline character
-            if (choice == 1) {
-                int sz = strlen(chars); 
-                newPassword = createPassWord(10, sz);
-                if (newPassword == NULL) {
-                    printf("Error generating password.\n");
-                    return false;
-                }
-                printf("Your new password is: %s.\n", newPassword);
-                } else if (choice == 2) {
-                    newPassword = (char *)malloc(200 * sizeof(char));
-                    if (newPassword == NULL) {
-                        printf("Memory allocation failed.\n");
-                        return false;
-                    }
-                printf("Enter the new password: ");
-                fgets(newPassword, 200, stdin);
-
-                size_t len = strlen(newPassword);
-                if (len > 0 && newPassword[len - 1] == '\n') {
-                    newPassword[len - 1] = '\0';
-                }
-            }
-            unsigned char newHash[CC_SHA256_DIGEST_LENGTH];
-            hashPassword(newPassword, newHash);
-            writeHashToFile("pass.txt", newHash);
-            loginHistory(true);
-            printf("Password changed successfully.\n");
-
-            if (newPassword != NULL) {
-                free(newPassword);  // Free allocated memory
-            }
-        }else printf("Program ended.\n");
-    }
-    return false;
-}
+#include <string.h>
+#include <stdbool.h>
 
 int main() {
-    int n = 2;
-    char **strings = (char **)malloc(n * sizeof(char *));
-    bool pass = true;
+    int n = 2;  // Number of strings to be input (password and string to encode)
+    char **strings = (char **)malloc(n * sizeof(char *));  // Allocate memory for storing `n` strings
+    bool pass = true;  // Flag to determine whether to prompt for password or the string to encode
     for (int i = 0; i < n; i++) {
-        strings[i] = (char *)malloc(300 * sizeof(char)); 
+        strings[i] = (char *)malloc(300 * sizeof(char));  // Allocate memory for each string
         if (pass) {
+            // Asks the user to enter password
             printf("Enter password: ");
-            fgets(strings[i], 200, stdin);
-            pass = !pass;
+            fgets(strings[i], 200, stdin);  // Read the password from stdin 
+            pass = !pass;  // Toggle the `pass` flag
         } else {
-            pass = !pass;
+            pass = !pass;  // Toggle the `pass` flag
+            // Prompt user to enter the string they want to encode
             printf("Enter the string you want to encode: ");
-            fgets(strings[i], 300, stdin);
+            fgets(strings[i], 300, stdin);  // Read the string to encode 
         }
 
+        // Remove the newline character from the end of the string, if present
         size_t len = strlen(strings[i]);
         if (len > 0 && strings[i][len - 1] == '\n') {
             strings[i][len - 1] = '\0';
         }
     }
-   if (checkPass(strings[0])) {
-        loginHistory(false);
-        implementHuffman(strings[1]);
+
+    // Check if the entered password is correct
+    if (checkPass(strings[0])) {
+        loginHistory(false);  // Write the login details in history.txt via library.c
+        implementHuffman(strings[1]);  // Performs Huffman encoding on the input string
     }
 
+    // Free allocated memory for the strings
     for (int i = 0; i < n; i++) {
         free(strings[i]);
     }
-    free(strings);
+    free(strings);  // Free the memory allocated for the array of strings
+
     return 0;
 }
 
